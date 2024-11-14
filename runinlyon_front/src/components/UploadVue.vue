9b8d88a5398557917b1fdfc095ecfc5b7e7ea3ee
benoit_collado <40575928+benoitCollado@users.noreply.github.com>
2024-11-14 16:16:18 +0000

    working on uploadView

--- runinlyon_front/src/components/UploadVue.vue
+++ runinlyon_front/src/components/UploadVue.vue
@@ -6,15 +6,46 @@
   //import {useRouter} from 'vue-router';
   let file : File;
   const fileReader = new FileReader();
+  let data : string;
+  let ready = false;
  function onChangeInput(e: Event){
     const target = e.target as InputFileEventTarget;
    file = target.files[0];
-   fileReader.readAsText(file);
-   fileReader.onloadend = () => {
-     console.log(fileReader.result);
-   }
-   console.log(file);
+   fileReader.readAsText(file,'latin1');
  }
+  fileReader.onloadend = () => {
+     let result = fileReader.result as string;
+     /*for(let i = 0; i < result.length; i++){
+       if(result[i] === 'é' || result[i] === 'è' || result[i] === 'ê'){
+         result[i] = 'e';
+       }
+       if(result[i] === 'à' || result[i] === 'â'){
+         result[i] = 'a';
+       }
+       if(result[i] === 'ù'){
+         result[i] = 'u';
+       }
+       if(result[i] === 'ç'){
+         result[i] = 'c';
+       }
+       if(result[i] === 'ï' || result[i] === 'î'){
+         result[i] = 'i';
+       }*/
+    result = result.replace(/é/g, 'e');
+    result = result.replace(/è/g, 'e');
+    result = result.replace(/ê/g, 'e');
+    result = result.replace(/à/g, 'a');
+    result = result.replace(/â/g, 'a');
+    result = result.replace(/ù/g, 'u');
+    result = result.replace(/ç/g, 'c');
+    result = result.replace(/ï/g, 'i');
+    result = result.replace(/î/g, 'i');
+    result = result.replace(/ô/g, 'o');
+       data = result;
+      ready = true;
+       console.log(data);
+      console.log(ready)
+   }
 </script>
 
 <template>
