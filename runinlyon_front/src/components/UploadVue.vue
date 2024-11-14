8abbf97560074d481223a953a2aba4a5e85fec78
benoit_collado <40575928+benoitCollado@users.noreply.github.com>
2024-11-14 15:48:59 +0000

    work on uploadView

--- runinlyon_front/src/components/UploadVue.vue
+++ runinlyon_front/src/components/UploadVue.vue
@@ -1,13 +1,21 @@
 <script setup lang="ts">
+  interface InputFileEventTarget extends EventTarget{
+    files: [File];
+  }
   //import {ref} from 'vue';
   //import {useRouter} from 'vue-router';
-
+  let file : File;
+ function onChangeInput(e: Event){
+    const target = e.target as InputFileEventTarget;
+   file = target.files[0];
+   console.log(file);
+ }
 </script>
 
 <template>
   <form>
     <label for="file">File</label>
-    <input type="file" id="file" name="file" />
+    <input @input="onChangeInput" type="file" id="file" name="file" />
     <button type="button" id="button">Upload</button>
   </form>
 </template>
